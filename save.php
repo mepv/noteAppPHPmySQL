09611f44064ff1ff9a2b5c91c3ec20a6a805bf1a
Mario Palacios <mepvdevelopments@gmail.com>
2021-03-03 13:06:11 -0300

    first commit

---
+++ save.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo '<pre>';
+//var_dump($_POST);
+//echo '</pre>';
+
+$connection = require_once './Connection.php';
+
+$id = $_POST['id'] ?? '';
+if ($id) {
+    $connection->updateNote($id, $_POST);
+} else {
+    $connection->addNote($_POST);
+}
+
+header('Location: index.php');
+
